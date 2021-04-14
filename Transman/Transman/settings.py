8d75eae75ddd901549762e1beb10b85545809f7f
Nantheshprabu <63918867+Nantheshprabu@users.noreply.github.com>
2021-04-14 11:46:36 +0530

    Update settings.py

--- Transman/Transman/settings.py
+++ Transman/Transman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8-ae_e+=c(4&avx#f2+5xxvwyvr22o-s+d#7lhj0o7@crvak4o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
